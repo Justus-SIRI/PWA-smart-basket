7c3b2d948c54c97bb54cfe7e5944cd7e122f1a3e
Justus Mwangi <mwangijustus12@gmail.com>
2024-07-20 05:03:37 +0300

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'smartbasket_pwa',
+  webDir: 'www'
+};
+
+export default config;
